07e118701ad9307e973593b22a23ed3fbe021344
SinaAbdollahi79 <sina.7979ab@gmail.com>
2024-02-21 16:31:33 +0330

    get first json

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,24 +2,23 @@ from . import views
 from django.urls import path,include
 from django.contrib import admin
 from rest_framework.documentation import include_docs_urls
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 
-"""
-URL configuration for mysite project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+schema_view = get_schema_view(
+   openapi.Info(
+      title="First API",
+      default_version='v1',
+      description="First api  description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 app_name = "mysite"
 
@@ -29,5 +28,7 @@ urlpatterns = [
     path("xxx/", views.PostTest.as_view(), name='post-test'),
     path('api-auth/', include('rest_framework.urls')),
     path('api-docs/',include_docs_urls(title='Superbook API')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('swagger/output.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 
 ]
